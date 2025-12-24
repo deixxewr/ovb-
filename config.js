c458fb9c13e68404b04d8e8f21f78ec0833b0679
deixxewr <islamove55@gmail.com>
2025-12-24 04:51:41 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const DISHES_API_URL = "https://edu.std-900.ist.mospolytech.ru/labs/api/dishes";
+// Альтернативный вариант для хостинга Мосполитеха:
+// const DISHES_API_URL = "http://lab7-api.std-900.ist.mospolytech.ru/api/dishes";
+
+const ORDERS_API_URL = "https://edu.std-900.ist.mospolytech.ru";
+// Альтернативный вариант:
+// const ORDERS_API_URL = "http://lab8-api.std-900.ist.mospolytech.ru";
+
+// ВАЖНО: замените на свой api_key перед отправкой преподавателю
+const API_KEY = "8a473e81-6a4b-4c04-84b8-271f6001040c";
